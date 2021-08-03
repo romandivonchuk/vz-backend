73b53fc6d62150d82895b316ae571f6237085c83
roman <roman.divonchuk@gmail.com>
2021-08-03 17:58:06 +0300

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require('express');
 const connectDB = require('./config/db');
 const productsRoutes = require('./routes/productsRoutes');
+const cors = reqire('cors')
 
 connectDB();
 
@@ -10,5 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/api/products', productsRoutes);
+
+app.use('/api/products', cors(), productsRoutes);
 app.listen(PORT, () => console.log(`server running on port ${PORT} `))
