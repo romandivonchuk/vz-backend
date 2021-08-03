7fa8999cb805bdbf61f3ad6172d3dd2567cf5443
roman <roman.divonchuk@gmail.com>
2021-08-03 18:02:43 +0300

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const express = require('express');
 const connectDB = require('./config/db');
 const productsRoutes = require('./routes/productsRoutes');
-const cors = reqire('cors')
+const cors = require('cors')
 
 connectDB();
 
